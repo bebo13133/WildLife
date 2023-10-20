bb9bd19b7222d97de96c946fa6219c882a0e98c7
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-10-20 08:56:30 +0300

    add

--- src/config/mongooseDB.js
+++ src/config/mongooseDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 //TODO: CHANGE DB NAME mongodb://127.0.0.1:27017/name - заместваме според задачата
-const uriPhotos = 'mongodb://127.0.0.1:27017/wildlife '
+const uriPhotos = 'mongodb+srv://bebo1313:bebo840512@bebo.5xjumgr.mongodb.net/?authMechanism=SCRAM-SHA-1'
 
 async function connectDB (){
 
